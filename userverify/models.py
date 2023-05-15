8c34313c8024ef19fc4ab9e4cb5e9c9b9f934823
Amirfazel <safazel@outlook.com>
2023-05-16 00:55:28 +0430

    ok

--- userverify/models.py
+++ userverify/models.py
@@ -4,6 +4,5 @@ from django.db import models
 class Userverify(models.Model):
     phone=models.TextField(max_length=20,blank=True)
     is_verified=models.BooleanField(default=False)
-    smscode=models.SmallIntegerField()
     active_code=models.BooleanField(default=False)
     pass
